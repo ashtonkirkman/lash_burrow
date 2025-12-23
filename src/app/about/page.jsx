7cc37ce8517cbcaf86d85d4ee0cdfbb83cc147d9
Ashton Kirkman <0604@asirobots.com>
2025-12-23 12:08:47 -0700

    Added picture of butterbean

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,11 @@
 export default function AboutPage() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col items-center justify-start bg-ivory text-brown px-6 py-26 space-y-24">
       <section className="max-w-3xl mx-auto space-y-4 grow">
-        <h2 className="text-3xl font-serif text-brown">
-          About The Lash Burrow
-        </h2>
+        {/* Page Title */}
+        <h1 className="text-6xl md:text-8xl font-fuzzy-bubbles text-brown text-center">
+          About
+        </h1>
         <p>
           At The Lash Burrow, we believe in enhancing your natural beauty in a relaxing,
           cottage-inspired space filled with warmth and charm.
@@ -12,6 +13,9 @@ export default function AboutPage() {
         <p>
           Founded by a passionate lash artist, our goal is to create an experience that feels as cozy as your favorite blanket and as beautiful as a spring garden.
         </p>
+        <image>
+          <img src="/bunnies/butterbean_1.jpg" alt="Butterbean the bunny" className="rounded-xl shadow-lg mt-6" />
+        </image>
       </section>
     </div>
   );
